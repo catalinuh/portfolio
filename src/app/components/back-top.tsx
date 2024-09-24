b67e14568aec542ccc76b5e3a439e330d54ae043
Catalina <catalinamcquade@gmail.com>
2024-09-23 23:06:20 -0400

    Finished conversion of jQuery to React for BackToTop component

--- src/app/components/back-top.tsx
+++ src/app/components/back-top.tsx
@@ -1,25 +1,46 @@
-import React, { useEffect, useState } from 'react'
+import React, { useCallback, useEffect, useState } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
 
 import './styles/back-top.scss'
 
 export default function BackToTop() {
   const [className, setClassName] = useState<string>('');
+  const [scrollTop, setScrollTop] = useState(0);
+
+  library.add(faChevronUp)
 
   useEffect(() => {
-    if (window?.scrollY > 100) {
-      setClassName('fadeIn')
-    } else {
-      setClassName('fadeOut')
-    }
-  }, [])
+    function onScroll() {
+      let currentPosition = window.scrollY;
+      if (currentPosition >= 100) {
+        setClassName('fadeIn')
+      } else {
+        setClassName('fadeOut')
+      }
 
-  const handleClickBackToTop = () => {
-    window?.scrollTo(0, 0)
-  }
+      setScrollTop(currentPosition <= 0 ? 0 : currentPosition);
+    }
+
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, [scrollTop]);
+
+  const handleClickBackToTop = useCallback(() => {
+    window.scrollTo(0, 0)
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener("click", handleClickBackToTop);
+    return () => {
+      window.removeEventListener("click", handleClickBackToTop);
+    };
+  }, [handleClickBackToTop]);
 
   return (
     <a href="#" className={`back-to-top animated ${className}`} onClick={handleClickBackToTop}>
-      <i className="fa fa-chevron-up"></i>
+      <FontAwesomeIcon icon="chevron-up" />
     </a>
   )
 }
